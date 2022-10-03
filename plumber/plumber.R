b76d9c48a6cefd90c8f7bdab2ba8863057319247
jesnorm <normand.jesse@gmail.com>
2022-10-03 14:04:53 -0400

    Sript updated

--- plumber/plumber.R
+++ plumber/plumber.R
@@ -1,6 +1,13 @@
 
 library(forecast)
+library(plumber)
+library(jsonlite)
+library(ggplot2)
+library(readr)
 
+options(scipen=999)
+
+model <- readRDS(("fit_nai.rds"))
 
 
 
@@ -13,17 +20,29 @@ function(){
 }
 
 
-model <- readRDS(("IC_arima_model2.rds"))
-
-#* Run model
-#* @serializer json
+#* Run model - Print Time Series Chart
+#* @serializer png
 #* @get  /prediction
 
 function(req, res){
   
+  mod <-forecast::forecast(model)
   
-  mod <- forecast(model)
+  print(autoplot(mod)+ labs(title = "Forecasted Revenue per Month",
+                            x="Year",
+                            y = "Revenue") + scale_y_continuous(labels=scales::dollar_format()))
+  
+
+}
+
+#* Pint Model Data
+#* @serializer json
+#* @get /print
+
+function(){
+  mod <-forecast::forecast(model)
   print(mod)
   
- 
+  
 }
+
